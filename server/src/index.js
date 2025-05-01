feb303808cbbe1ba0c8ff68c7a81103d3bbb33d9
mayurpatil-13 <mp9112627825@gmail.com>
2025-05-01 14:37:14 +0530

    Added cors and cookie Parser

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env.local'
@@ -7,4 +8,16 @@ dotenv.config({
 
 console.log(`ENV variables ${process.env.MONGODB_CONNECTION_STRING}`);
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (error) =>{
+        console.log("Error after mongo connection" , error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 5000 , () =>{
+        console.log("app is listening")
+    })
+})
+.catch((err) => {
+    console.log(`mongodb connection failed ${err}`);
+})
